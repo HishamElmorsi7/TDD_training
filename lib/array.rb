9695adf89c4ec81e8830ed42d5a7ea4be6a9c9f4
HishamElmorsi7 <hi7u3xe@gmail.com>
2022-10-05 23:49:31 +0200

    array tests and methods implementation

---
+++ lib/array.rb
@@ -0,0 +1,62 @@
+def uniq(arr)
+
+    uniq_arr = []
+    arr.each do |ele|
+        uniq_arr << ele unless uniq_arr.include?(ele)
+    end
+
+    uniq_arr
+end
+
+def two_sum(arr)
+    two_sum_pairs = []
+    arr.each_with_index do |ele1, i1|
+        arr.each_with_index do |ele2, i2|
+            two_sum_pairs << [i1, i2] if ele1 + ele2 == 0 && i1 != i2
+        end
+    end
+
+    # I have repeated pair and here I remove them
+    limit_end =  two_sum_pairs.length / 2 - 1
+    two_sum_pairs[ 0.. limit_end]
+end
+
+def my_transpose(arr)
+    cols = []
+
+    arr.each_with_index do |row, i|
+        new_row = []
+        row.each_with_index do |ele, j|
+            new_row << arr[j][i]
+        end
+        cols << new_row
+    end
+
+    cols
+end
+
+# stock picker method
+
+def stock_pick(price_per_days)
+
+    lowest_price = Float::INFINITY
+    highest_price = 0
+
+    lowest_price_day = ''
+    highest_price_day = ''
+
+    price_per_days.each do |key, val|
+        if( val < lowest_price )
+            lowest_price = val
+            lowest_price_day = key 
+        end
+
+        if( val >= highest_price )
+            highest_price = val
+            highest_price_day = key 
+        end
+
+    end
+
+    [lowest_price_day, highest_price_day]
+end
---
+++ spec/array_spec.rb
@@ -0,0 +1,57 @@
+require 'rspec'
+require 'array'
+
+describe 'uniq' do
+    let(:array) { [1, 2, 1, 3, 3] }
+    let(:uniq_arr) { uniq(array.dup) }
+
+    it "doesn't have duplicates" do
+        array.each do |ele|
+            expect( uniq_arr.count(ele) ).to eq(1)
+        end
+    end
+
+    it "doesn't have elements from outside the original array" do
+        uniq_arr.each do |ele|
+            expect(array).to include(ele)
+        end
+    end
+
+    it "doesn't change the original array and results a new arr" do
+        expect{ uniq(array) }.to_not change{ array }
+    end
+
+end
+
+describe 'two_sums' do
+    it 'finds all pairs of positions where the elements at those positions sum to zero' do
+        expect(two_sum( [-1, 0, 2, -2, 1] )).to eq( [[0, 4], [2, 3]])
+    end
+end
+
+describe 'my_transpose' do
+    it 'will convert between the row-oriented and column-oriented representations' do
+        rows = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8]
+        ]
+
+        cols = [
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8]
+        ]
+
+        expect(my_transpose(rows)).to eq(cols)
+    end
+end
+
+describe 'stock_pick' do
+    it 'takes an array of stock prices and outputs the most profitable days to first buy the stock and then sell the stock' do
+
+        stock_price_per_days = { 'Day 0' => 1000, 'Day 1' => 500, 'Day 2' => 2000, 'Day 3' => 5000, 'Day 4' => 600, 'Day 5' => 7000, 'Day 6' => 3000 }
+        expect(stock_pick(stock_price_per_days)).to eq(['Day 1', 'Day 5'])
+
+    end
+end
